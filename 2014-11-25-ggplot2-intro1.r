ca8b50547dacd4a94bc58dc8c55f13bae8825f8d
aaronwolen <aaron@wolen.com>
2014-11-25 12:41:38 -0500

    Initial commit

---
+++ 2014-11-25-ggplot2-intro1.r
@@ -0,0 +1,9 @@
+# ggplot2 introduction 1
+
+
+# Load data ---------------------------------------------------------------
+
+disease <- read.csv("data/disease.csv")
+
+# Verify data looks as expected
+head(disease)
